985d49d4b941013e27fc340d03c5e7a20199c53c
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-24 23:17:21 -0500

    fc

---
+++ 01_intro/hello-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello React!</h1>
+     <h2>Hello React again!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_intro/simplest-react-cdn-example.js
@@ -0,0 +1,6 @@
+const element = React.createElement('h1', null, 'Hello React!');
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
