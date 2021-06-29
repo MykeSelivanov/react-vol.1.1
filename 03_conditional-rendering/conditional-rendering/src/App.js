45bff36d66f1beab007c1df343c9499f31c510ba
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-28 20:48:15 -0500

    conditional rendering

--- 03_conditional-rendering/conditional-rendering/src/App.js
+++ 03_conditional-rendering/conditional-rendering/src/App.js
@@ -5,6 +5,7 @@ function App() {
   const theme = 'dark';
 
   return (
+    // eslint-disable-next-line react/jsx-no-duplicate-props
     <div className="App" className={ theme === 'dark' ? 'App dark' : 'App light' }>
       <header className="App-header">
         <h1>Conditional rendering</h1>
