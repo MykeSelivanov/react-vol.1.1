d0bfb509532ae3d6c2dfe161594e87bc66a975c2
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-01 20:16:07 -0500

    react router

--- 06_react-router/react-router/src/App.js
+++ 06_react-router/react-router/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, HashRouter, Route } from 'react-router-dom';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+      </div>
+
+    <Route exact path="/" component={Home}/>
+    <Route path="/about" component={About}/>
+    </Router>
   );
 }
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+
 
 export default App;
